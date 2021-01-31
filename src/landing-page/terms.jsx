832654a0a991ca318cbe4e53bed3d7435541caf8
mk210 <55575735+mk210@users.noreply.github.com>
2021-01-31 10:31:52 +0400

    updated terms

--- src/landing-page/terms.jsx
+++ src/landing-page/terms.jsx
@@ -175,6 +175,265 @@ export default function Terms() {
                   device.
                 </p>{" "}
                 <p>
+                  <strong>INTELLECTUAL PROPERTY RIGHTS</strong>
+                </p>
+                <p>
+                  Unless otherwise indicated, the Application is our proprietary
+                  property and all source code, databases, functionality,
+                  software, website designs, audio, video, text, photographs,
+                  and graphics on the Application (collectively, the “Content”)
+                  and the trademarks, service marks, and logos contained therein
+                  (the “Marks”) are owned or controlled by us or licensed to us,
+                  and are protected by copyright and trademark laws and various
+                  other intellectual property rights and unfair competition laws
+                  of the United Arab Emirates, foreign jurisdictions, and
+                  international conventions. The Content and the Marks are
+                  provided on the Application “AS IS” for your information and
+                  personal use only. Except as expressly provided in these Terms
+                  of Use, no part of the Application and no Content or Marks may
+                  be copied, reproduced, aggregated, republished, uploaded,
+                  posted, publicly displayed, encoded, translated, transmitted,
+                  distributed, sold, licensed, or otherwise exploited for any
+                  commercial purpose whatsoever, without our express prior
+                  written permission. Provided that you are eligible to use the
+                  Application, you are granted a limited license to access and
+                  use the Application and to download or print a copy of any
+                  portion of the Content to which you have properly gained
+                  access solely for your personal, non-commercial use. We
+                  reserve all rights not expressly granted to you in and to the
+                  Application, Content, and the Marks.
+                </p>
+                <p>
+                  <strong>USER REPRESENTATIONS</strong>
+                </p>
+                <p>
+                  By using the Application, you represent and warrant that: (1)
+                  all registration information you submit will be true,
+                  accurate, current, and complete; (2) you will maintain the
+                  accuracy of such information and promptly update such
+                  registration information as necessary; (3) you have the legal
+                  capacity and you agree to comply with these Terms of Use; (4)
+                  you are not under the age of 13; (5) not a minor in the
+                  jurisdiction in which you reside, or if a minor, you have
+                  received parental permission to use the Site; (6) you will not
+                  access the Application through automated or non-human means,
+                  whether through a bot, script or otherwise; (7) you will not
+                  use the Application for any illegal or unauthorized purpose;
+                  and (8) your use of the Application will not violate any
+                  applicable law or regulation. If you provide any information
+                  that is untrue, inaccurate, not current, or incomplete, we
+                  have the right to suspend or terminate your account and refuse
+                  any and all current or future use of the Application(or any
+                  portion thereof).
+                </p>
+                <p>
+                  <strong>USER REGISTRATION</strong>
+                </p>
+                <p>
+                  You may be required to register with the Application. You
+                  agree to keep your password confidential and will be
+                  responsible for all use of your account and password. We
+                  reserve the right to remove, reclaim, or change a username you
+                  select if we determine, in our sole discretion, that such
+                  username is inappropriate, obscene, or otherwise
+                  objectionable.
+                </p>
+                <p>
+                  <strong>PROHIBITED ACTIVITIES</strong>
+                </p>
+                <p>
+                  You may not access or use the Application for any purpose
+                  other than that for which we make the Application available.
+                  The Application may not be used in connection with any
+                  commercial endeavors except those that are specifically
+                  endorsed or approved by us. As a user of the Application, you
+                  agree not to: 1. Systematically retrieve data or other content
+                  from the Application to create or compile, directly or
+                  indirectly, a collection, compilation, database, or directory
+                  without written permission from us. 2. Make any unauthorized
+                  use of the Application, including collecting usernames and/or
+                  email addresses of users by electronic or other means for the
+                  purpose of sending unsolicited email, or creating user
+                  accounts by automated means or under false pretenses. 3. Use a
+                  buying agent or purchasing agent to make purchases on the
+                  Application. 4. Use the Application to advertise or offer to
+                  sell goods and services. 5. Circumvent, disable, or otherwise
+                  interfere with security-related features of the Application,
+                  including features that prevent or restrict the use or copying
+                  of any Content or enforce limitations on the use of the
+                  Application and/or the Content contained therein. 6. Engage in
+                  unauthorized framing of or linking to the Application. 7.
+                  Trick, defraud, or mislead us and other users, especially in
+                  any attempt to learn sensitive account information such as
+                  user passwords; 8. Make improper use of our support services
+                  or submit false reports of abuse or misconduct. 9. Engage in
+                  any automated use of the system, such as using scripts to send
+                  comments or messages, or using any data mining, robots, or
+                  similar data gathering and extraction tools. 10. Interfere
+                  with, disrupt, or create an undue burden on the Application or
+                  the networks or services connected to the Application. 11.
+                  Attempt to impersonate another user or person or use the
+                  username of another user. 12. Sell or otherwise transfer your
+                  profile. 13. Use any information obtained from the Application
+                  in order to harass, abuse, or harm another person. 14. Use the
+                  Application as part of any effort to compete with us or
+                  otherwise use the Application and/or the Content for any
+                  revenue-generating endeavor or commercial enterprise. 15.
+                  Decipher, decompile, disassemble, or reverse engineer any of
+                  the software comprising or in any way making up a part of the
+                  Application. 16. Attempt to bypass any measures of the
+                  Application designed to prevent or restrict access to the
+                  Application, or any portion of the Application. 17. Harass,
+                  annoy, intimidate, or threaten any of our employees or agents
+                  engaged in providing any portion of the Application to you.
+                  18. Delete the copyright or other proprietary rights notice
+                  from any Content. 19. Copy or adapt the Application’s
+                  software, including but not limited to Flash, PHP, HTML,
+                  JavaScript, or other code. 20. Upload or transmit (or attempt
+                  to upload or to transmit) viruses, Trojan horses, or other
+                  material, including excessive use of capital letters and
+                  spamming (continuous posting of repetitive text), that
+                  interferes with any party’s uninterrupted use and enjoyment of
+                  the Application or modifies, impairs, disrupts, alters, or
+                  interferes with the use, features, functions, operation, or
+                  maintenance of the Application. 21. Upload or transmit (or
+                  attempt to upload or to transmit) any material that acts as a
+                  passive or active information collection or transmission
+                  mechanism, including without limitation, clear graphics
+                  interchange formats (“gifs”), 1×1 pixels, web bugs, cookies,
+                  or other similar devices (sometimes referred to as “spyware”
+                  or “passive collection mechanisms” or “pcms”). 22. Except as
+                  may be the result of standard search engine or Internet
+                  browser usage, use, launch, develop, or distribute any
+                  automated system, including without limitation, any spider,
+                  robot, cheat utility, scraper, or offline reader that accesses
+                  the Application, or using or launching any unauthorized script
+                  or other software. 23. Disparage, tarnish, or otherwise harm,
+                  in our opinion, us and/or the Application. 24. Use the
+                  Application in a manner inconsistent with any applicable laws
+                  or regulations.
+                </p>
+                <p>
+                  <strong>USER GENERATED CONTRIBUTIONS</strong>
+                </p>
+                <p>
+                  The Application may invite you to chat, contribute to, or
+                  participate in blogs, message boards, online forums, play
+                  games and other functionality, and may provide you with the
+                  opportunity to create, submit, post, display, transmit,
+                  perform, publish, distribute, or broadcast content and
+                  materials to us or on the Application, including but not
+                  limited to text, writings, video, audio, photographs,
+                  graphics, comments, suggestions, or personal information or
+                  other material (collectively, “Contributions”). Contributions
+                  may be viewable by other users of the Application and through
+                  third-party websites. As such, any Contributions you transmit
+                  may be treated as non-confidential and non-proprietary. When
+                  you create or make available any Contributions, you thereby
+                  represent and warrant that: 1. The creation, distribution,
+                  transmission, public display, or performance, and the
+                  accessing, downloading, or copying of your Contributions do
+                  not and will not infringe the proprietary rights, including
+                  but not limited to the copyright, patent, trademark, trade
+                  secret, or moral rights of any third party. 2. You are the
+                  creator and owner of or have the necessary licenses, rights,
+                  consents, releases, and permissions to use and to authorize
+                  us, the Application, and other users of the Application to use
+                  your Contributions in any manner contemplated by the
+                  Application and these Terms of Use. 3. You have the written
+                  consent, release, and/or permission of each and every
+                  identifiable individual person in your Contributions to use
+                  the name or likeness of each and every such identifiable
+                  individual person to enable inclusion and use of your
+                  Contributions in any manner contemplated by the Application
+                  and these Terms of Use. 4. Your Contributions do not include
+                  images of person(s) 5. Your Contributions are not false,
+                  inaccurate, or misleading. 6. Your Contributions are not
+                  unsolicited or unauthorized advertising, promotional
+                  materials, pyramid schemes, chain letters, spam, mass
+                  mailings, or other forms of solicitation. 7. Your
+                  Contributions are not obscene, lewd, lascivious, filthy,
+                  violent, harassing, libelous, slanderous, or otherwise
+                  objectionable (as determined by us). 8. Your Contributions do
+                  not ridicule, mock, disparage, intimidate, or abuse anyone. 9.
+                  Your Contributions do not advocate the violent overthrow of
+                  any government or incite, encourage, or threaten physical harm
+                  against another. 10. Your Contributions do not violate any
+                  applicable law, regulation, or rule. 11. Your Contributions do
+                  not violate the privacy or publicity rights of any third
+                  party. 12. Your Contributions do not contain any material that
+                  solicits personal information from anyone under the age of 18
+                  or exploits people under the age of 18 in a sexual or violent
+                  manner. 13. Your Contributions do not violate any federal or
+                  state law concerning child pornography, or otherwise intended
+                  to protect the health or well-being of minors; 14. Your
+                  Contributions do not include any offensive comments that are
+                  connected to race, national origin, gender, sexual preference,
+                  or physical handicap. 15. Your Contributions do not otherwise
+                  violate, or link to material that violates, any provision of
+                  these Terms of Use, or any applicable law or regulation. Any
+                  use of the Application in violation of the foregoing violates
+                  these Terms of Use and may result in, among other things,
+                  termination or suspension of your rights to use the
+                  Application.
+                </p>
+                <p>
+                  <strong>CONTRIBUTION LICENSE</strong>
+                </p>
+                <p>
+                  By posting your Contributions to any part of the Application,
+                  or making Contributions accessible to the Application by
+                  linking your account from the Application to any of your
+                  social networking accounts, you automatically grant, and you
+                  represent and warrant that you have the right to grant, to us
+                  an unrestricted, unlimited, irrevocable, perpetual,
+                  non-exclusive, transferable, royalty-free, fully-paid,
+                  worldwide right, and license to host, use, copy, reproduce,
+                  disclose, sell, resell, publish, broadcast, retitle, archive,
+                  store, cache, publicly perform, publicly display, reformat,
+                  translate, transmit, excerpt (in whole or in part), and
+                  distribute such Contributions (including, without limitation,
+                  your image and voice) for any purpose, commercial,
+                  advertising, or otherwise, and to prepare derivative works of,
+                  or incorporate into other works, such Contributions, and grant
+                  and authorize sublicenses of the foregoing. The use and
+                  distribution may occur in any media formats and through any
+                  media channels. This license will apply to any form, media, or
+                  technology now known or hereafter developed, and includes our
+                  use of your name, company name, and franchise name, as
+                  applicable, and any of the trademarks, service marks, trade
+                  names, logos, and personal and commercial images you provide.
+                  You waive all moral rights in your Contributions, and you
+                  warrant that moral rights have not otherwise been asserted in
+                  your Contributions.
+                </p>
+                <p>
+                  <strong>
+                    <i>
+                      We do not assert any ownership over your Contributions.
+                      You retain full ownership of all of your Contributions and
+                      any intellectual property rights or other proprietary
+                      rights associated with your Contributions. We are not
+                      liable for any statements or representations in your
+                      Contributions provided by you in any area on the
+                      Application. You are solely responsible for your
+                      Contributions to the Application and you expressly agree
+                      to exonerate us from any and all responsibility and to
+                      refrain from any legal action against us regarding your
+                      Contributions.
+                    </i>
+                  </strong>
+                </p>
+                <p>
+                  We have the right, in our sole and absolute discretion, (1) to
+                  edit, redact, or otherwise change any Contributions; (2) to
+                  re-categorize any Contributions to place them in more
+                  appropriate locations on the Application; and (3) to
+                  pre-screen or delete any Contributions at any time and for any
+                  reason, without notice. We have no obligation to monitor your
+                  Contributions.
+                </p>
+                <p>
                   <strong>Changes to This Terms and Conditions</strong>
                 </p>{" "}
                 <p>
