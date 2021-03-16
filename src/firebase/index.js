2828fe907230371968707c29a8eae5d59a945a83
GliderCodes <thegoglider@gmail.com>
2021-03-16 23:38:46 +0400

    firebase initialized

---
+++ src/firebase/index.js
@@ -0,0 +1,22 @@
+import 'firebase/database';
+import firebase from 'firebase';
+import 'dot-env';
+// import '@firebase/messaging';
+//import './sign-in screen';
+const config = {
+    apiKey: "AIzaSyDIvlHZc4WMB_6IibnMOQc-_D9M2noB57w",
+    authDomain: "geopick-db.firebaseapp.com",
+    databaseURL: "https://geopick-db-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "geopick-db",
+    storageBucket: "geopick-db.appspot.com",
+    messagingSenderId: "G-XS2DD50LZW",
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+export default db;
+// use -> import db from './firebase' 
+// from root to use this db collection
